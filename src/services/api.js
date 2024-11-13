070ff7529d7f93a8886c8a7aa2c910c2aef4788c
bishalnath1 <bishal@icsportal.in>
2024-11-13 13:10:55 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,33 @@
 import axios from 'axios';
 // const axios = require('axios');
 export const submitFeedback = (dataabc) => {
-    // let url="https://insurvey.sendmsg.in/mobileicsmobile/AddMessage.php";
-    // const response = axios.post(url, dataabc);
-const targetUrl = 'https://insurvey.sendmsg.in/mobileicsmobile/AddMessage.php';
-const response = axios.post(targetUrl, dataabc, {
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-});
+    let data = JSON.stringify({
+        "api_key": "QtOaDjEZSJsHUvsuxcuKab",
+        "email": "kabaleeswaran110@gmail.com",
+        "first_name": "kabali",
+        "last_name": "eswaran",
+        "companySize": "11-50",
+        "phone_number": "6369323238",
+        "helpOptions": "improve-communication"
+      });
+      
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://insurvey.sendmsg.in/mobileicsmobile/AddMessage.php',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+      
+      axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     console.log(response);
     return false;
 };
